03e4d8fdde7b586edd322090093be52f9733ebf8
korchinmv <korchinmv@yandex.ru>
2023-08-17 11:34:40 +0300

    fix: ревью №2

--- controllers/users.js
+++ controllers/users.js
@@ -1,21 +1,27 @@
-const bcrypt = require('bcryptjs');
-const userModel = require('../models/user');
+const bcrypt = require("bcryptjs");
+const userModel = require("../models/user");
 const {
-  messageNotUser, messageEmail, messageDataError, messageErrorEmailOrPassword, CREATED,
-} = require('../utils/responses');
+  messageNotUser,
+  messageEmail,
+  messageDataError,
+  messageErrorEmailOrPassword,
+  CREATED,
+} = require("../utils/responses");
 
-const { generateToken } = require('../utils/jwtAuth');
-const NotFoundError = require('../errors/NotFoundError');
-const ValidationError = require('../errors/ValidationError');
-const ConflictError = require('../errors/ConflictError');
-const UnauthorizationError = require('../errors/UnauthorizationError');
+const { generateToken } = require("../utils/jwtAuth");
+const NotFoundError = require("../errors/NotFoundError");
+const ValidationError = require("../errors/ValidationError");
+const ConflictError = require("../errors/ConflictError");
+const UnauthorizationError = require("../errors/UnauthorizationError");
 
 const getUser = async (req, res, next) => {
   try {
-    const user = await userModel.findById(req.user._id).orFail(new NotFoundError(messageNotUser));
+    const user = await userModel
+      .findById(req.user._id)
+      .orFail(new NotFoundError(messageNotUser));
     res.send({ email: user.email, name: user.name });
   } catch (error) {
-    if (error.name === 'CastError') {
+    if (error.name === "CastError") {
       return next(new ValidationError(messageDataError));
     }
     next(error);
@@ -25,13 +31,22 @@ const getUser = async (req, res, next) => {
 
 const updateUser = async (req, res, next) => {
   try {
-    const updatedUser = await userModel.findByIdAndUpdate(req.user._id, req.body, { new: true, runValidators: true }).orFail(new Error('NotValidData'));
+    const updatedUser = await userModel
+      .findByIdAndUpdate(req.user._id, req.body, {
+        new: true,
+        runValidators: true,
+      })
+      .orFail(new Error("NotValidData"));
     res.send({ email: updatedUser.email, name: updatedUser.name });
   } catch (error) {
-    if (error.message === 'NotValidData') {
+    if (error.message === "NotValidData") {
       return next(new NotFoundError(messageDataError));
-    } if (error.name === 'ValidationError') {
+    }
+    if (error.name === "ValidationError") {
       return next(new ValidationError(messageDataError));
+    }
+    if (error.code === 11000) {
+      return next(new ConflictError(messageEmail));
     }
     next(error);
   }
@@ -39,9 +54,7 @@ const updateUser = async (req, res, next) => {
 };
 
 const createUser = async (req, res, next) => {
-  const {
-    email, password, name,
-  } = req.body;
+  const { email, password, name } = req.body;
   const hashPassword = await bcrypt.hash(password, 12);
   try {
     const user = await userModel.create({
@@ -53,9 +66,10 @@ const createUser = async (req, res, next) => {
     delete updUser.password;
     res.status(CREATED).send({ data: updUser });
   } catch (error) {
-    if (error.name === 'ValidationError') {
+    if (error.name === "ValidationError") {
       return next(new ValidationError(messageDataError));
-    } if (error.code === 11000) {
+    }
+    if (error.code === 11000) {
       return next(new ConflictError(messageEmail));
     }
     next(error);
@@ -66,15 +80,19 @@ const createUser = async (req, res, next) => {
 const loginUser = async (req, res, next) => {
   const { email, password } = req.body;
   try {
-    const user = await userModel.findOne({ email }).select('+password').orFail(new Error('UnauthorizedError'));
+    const user = await userModel
+      .findOne({ email })
+      .select("+password")
+      .orFail(new Error("UnauthorizedError"));
     const match = await bcrypt.compare(password, user.password);
     if (!match) {
       return next(new UnauthorizationError(messageErrorEmailOrPassword));
     }
-    const token = generateToken({ _id: user._id }, '7d');
+    const token = generateToken({ _id: user._id }, "7d");
     res.send({ token });
   } catch (error) {
-    if (error.message === 'UnauthorizedError') return next(new UnauthorizationError(messageErrorEmailOrPassword));
+    if (error.message === "UnauthorizedError")
+      return next(new UnauthorizationError(messageErrorEmailOrPassword));
     next(error);
   }
   return null;
